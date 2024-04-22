66fc0a7927c3256af9d65806e80f6b1616d327f8
Krison Nilo <krisonnilo@gmail.com>
2024-04-22 21:12:57 +0200

    qwer

--- src/pages/api/get-quote.js
+++ src/pages/api/get-quote.js
@@ -2,7 +2,6 @@ export default async function name(req, res) {
 	await fetch('https://zenquotes.io/api/today')
 		.then(res => res.json())
 		.then(data => {
-			console.log(data)
 			res.status(201).json({
 				author: data[0].a,
 				quote: data[0].q,
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,10 @@ import UserContext from '@/store/user-context'
 import SearchText from '@/components/UI/SearchText'
 import classes from '@/styles/Home.module.css'
 import animate from '@/hooks/animate'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+	const router = useRouter()
 	const userContext = useContext(UserContext)
 	const [searching, setSearching] = useState(false)
 	const [classname, setClassname] = useState('slide-left')
@@ -14,39 +16,32 @@ export default function Home() {
 	const notes = searching ? filterdNotes : userContext.user.notes
 
 	useEffect(() => {
-		document.body.style.backgroundColor = userContext.bgColor
-		document.body.style.color = userContext.color
-	}, [userContext.user])
-
-	useEffect(() => {
-		document.body.style.backgroundColor = userContext.bgColor
-		document.body.style.color = userContext.color
-		userContext.showNoteBtnHandler(true)
-		const prevPath = sessionStorage.getItem('prevPath')
-
-		if (prevPath === '/user') {
-			setClassname('slide-right')
-		}
-		if (prevPath !== '/user' && prevPath !== '/') {
-			setClassname('slide-left')
-		}
-		sessionStorage.setItem('prevPath', window.location.pathname)
-		const id = sessionStorage.getItem('noteId')
-		if (id) {
-			console.log('id: ', id)
-			animate(id).shrink('1s')
-			setTimeout(() => {
-				deleteNoteHandler(id)
-				sessionStorage.removeItem('noteId')
-			}, 1500)
-		}
-	}, [])
-
-	// get notes..
-	useEffect(() => {
 		const user = JSON.parse(sessionStorage.getItem('user'))
 		if (user) {
 			userContext.userHandler(user)
+			document.body.style.backgroundColor = userContext.bgColor
+			document.body.style.color = userContext.color
+			userContext.showNoteBtnHandler(true)
+			const prevPath = sessionStorage.getItem('prevPath')
+
+			if (prevPath === '/user') {
+				setClassname('slide-right')
+			}
+			if (prevPath !== '/user' && prevPath !== '/') {
+				setClassname('slide-left')
+			}
+			sessionStorage.setItem('prevPath', window.location.pathname)
+			const id = sessionStorage.getItem('noteId')
+			if (id) {
+				console.log('id: ', id)
+				animate(id).shrink('1s')
+				setTimeout(() => {
+					deleteNoteHandler(id)
+					sessionStorage.removeItem('noteId')
+				}, 1500)
+			}
+		} else {
+			router.push('/login')
 		}
 	}, [])
 
